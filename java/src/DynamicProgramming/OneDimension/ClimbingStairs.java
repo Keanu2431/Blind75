50dfe78ae1da7a813896115c0ebe1f964c472af9
Keanu2431 <lumpkinkeanu131@gmail.com>
2023-04-19 01:23:35 -0400

    .

--- java/src/DynamicProgramming/OneDimension/ClimbingStairs.java
+++ java/src/DynamicProgramming/OneDimension/ClimbingStairs.java
@@ -8,6 +8,8 @@ public class ClimbingStairs {
         dp[0] = 1;
         dp[1] = 1;
         for (int i = 2; i <= n; i++)
+//            ANSWER IS THE ADDITION OF THE PREVIOUS ELEMENT AND THE DOUBLE PREVIOUS ELEMENT
+//            OF THE CURRENT ITERATION
             dp[i] = dp[i - 1] + dp[i - 2];
         System.out.println(Arrays.toString(dp));
         return dp[n];
